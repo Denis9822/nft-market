1363a3fac0c18e0a0e8f1cda3e7b860b7e649473
Denys <ss.set2013@gmail.com>
2022-04-29 22:26:01 +0300

    update

--- pages/index.vue
+++ pages/index.vue
@@ -213,7 +213,7 @@
     </div>
     <div class="slider_lets-go mob">
       <div class="slider_wrap">
-        <carousel v-if="!loading" :perPage="1" :paginationPadding="paggPadding" :navigationEnabled="true">
+        <carousel v-if="!loading" :perPage="1" :paginationPadding="paggPadding" >
           <slide>
             <div class="slider_item si1">
               <div class="wrap">
